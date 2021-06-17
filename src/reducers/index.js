1a8bb126c1a920e1398e867bd010afb92a755fab
KBENBENECK <kbenbeneck@gmail.com>
2021-06-17 18:11:05 -0400

    Add static songsReducer and selectedSongReducer funcs

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+const songsReducer = () => {
+    return [
+        {title: 'Song 1', duration: '1:11'},
+        {title: 'Song 2', duration: '2:22'},
+        {title: 'Song 3', duration: '3:33'},
+        {title: 'Song 4', duration: '4:44'}
+    ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload;
+    }
+    return selectedSong;
+};
